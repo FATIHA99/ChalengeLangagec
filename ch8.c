e2234727161651e48de5c2279a6ad07724b3b59f
FATIHA99 <fatihhaa27@gmail.com>
2021-11-12 16:46:06 +0100

    Les chalenges

--- ch8.c
+++ ch8.c
@@ -6,10 +6,13 @@
 int main(int argc, char *argv[]) {
 	int nbr1,nbr2,nbr3,nbr4,somme;
  float moyen;
+ 
  printf("entrer les quatre valeur \n");
  scanf("%d %d %d %d",&nbr1,&nbr2,&nbr3,&nbr4);
+ 
+ 
   somme=nbr1+nbr2+nbr3+nbr4;
-  moyen=somme/4;
+  moyen=somme/4.0;
   printf("la somme:  %d \n le moyen :%.2f \n",somme,moyen);
 	return 0;
 }
--- ch9.c
+++ ch9.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+//#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
   printf("entrer les coordonner de la deuxieme  point  (N)\n");
  scanf("%f %f",&x2,&y2);
 
- distance=sqrt(pow(x1-x2,2)+pow(y2-y1,2));
+//// distance=sqrt(pow(x2-x1,2)+pow(y2-y1,2));
  printf("la distance entre M(%f,%f) et N(%f,%f) est %f \n",x1,y1,x2,y2,distance);
 
 return 0;
